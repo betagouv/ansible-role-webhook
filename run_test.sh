e9d046256820965d7815565dc1bd8410a87d2cc9
Anders Sildnes <andsild@posteo.net>
2020-06-25 12:52:06 +0200

    Fix bug with httphooks

--- run_test.sh
+++ run_test.sh
@@ -32,5 +32,5 @@ errors=0
 for case in $cases
 do
     ansible-playbook ./tests/test.yml -i 127.0.0.1, -t $case -e prefix_dir="cases/${case}" --ask-become-pass
-    test -d  ./tests/cases/case-1/var && sudo rm -rv ./tests/cases/case-1/var
+    #test -d  ./tests/cases/case-1/var && sudo rm -rv ./tests/cases/case-1/var
 done
